138e2bab090213b8451b97c000b1adcbb3c0e5e2
Ivan Montiel <idmontie@asu.edu>
2015-05-02 00:08:42 -0700

    Create emotions-data.js
    Initial emotion data. Started adding "Bad" emotions.

---
+++ src/emotions-data.js
@@ -0,0 +1,50 @@
+this._emotions_data = [
+  {
+    slug : 'bad',
+    readible : 'Bad',
+    parent : null,
+    tier : 0
+  },
+  {
+    slug : 'bored',
+    readible : 'Bored',
+    parent : 'bad',
+    tier : 1
+  },
+  {
+    slug : 'indifferent',
+    readible : 'Indifferent',
+    parent 'bored',
+    tier : 2
+  },
+  {
+    slug : 'apathetic',
+    readible : 'Apathetic',
+    parent : 'bored',
+    tier : 2
+  },
+  {
+    slug : 'busy',
+    readible : 'Busy',
+    parent : 'bad',
+    tier : 1
+  },
+  {
+    slug : 'pressured',
+    readible : 'Pressured',
+    parent : 'busy',
+    tier : 2
+  },
+  {
+    slug : 'rushed',
+    readible : 'Rushed',
+    parent : 'busy',
+    tier : 2
+  },
+  {
+    slug : 'stressed',
+    readible : 'Stressed',
+    parent : 'bad',
+    tier : 1,
+  }
+];
